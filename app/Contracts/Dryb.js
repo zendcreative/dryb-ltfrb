4ea337a8532b7af28efa58f3931d596509e60768
Zend Developer <hello@zendcreative.com>
2018-03-28 18:27:44 +0800

    Added comments

--- app/Contracts/Dryb.js
+++ app/Contracts/Dryb.js
@@ -1,4 +1,4 @@
 const web3 = use('App/Ethereum/Web3')
 const abi = require('./abi/dryb')
 
-module.exports = new web3.eth.Contract(abi, '0xb529f14aa8096f943177c09ca294ad66d2e08b1f')
+module.exports = new web3.eth.Contract(abi, '0xb529f14aa8096f943177c09ca294ad66d2e08b1f' /* contract address */)
